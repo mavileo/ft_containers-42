fab5950b333161fdd7d92fc2f583cb8b02cdac7f
mavileo <mavileo@student.42.fr>
2021-06-10 17:45:05 +0200

    map beginning

--- main.cpp
+++ main.cpp
@@ -2,62 +2,28 @@
 #include <list>
 #include <string>
 #include <iterator>
-#include "vector.hpp"
+#include "map.hpp"
 
-void	print_vect(ft::vector<int> vect) {
-	ft::vector<int>::iterator it = vect.begin();
-	while (it != vect.end()) {
-		std::cout << *it << std::endl;
-		it++;
-	}
+void	print_map(ft::map<std::string, int> map) {
+	std::cout << map.get_val() << std::endl;
 	std::cout << "\n";
 }
 
-void	debug_vect(ft::vector<int> vect) {
-	ft::vector<int>::iterator it = vect.begin();
-	size_t i = 0;
-	if (vect.size() == 0)
-		return ;
-	while (i < vect.size()) {
-		std::cout << i << "/" << vect.size() << std::endl;
-		std::cout << *it << std::endl;
-		std::cout << std::endl;
-		it++;
-		i++;
-	}
-	std::cout << i << "/" << vect.size() << std::endl;
-	std::cout << *it << std::endl;
-	std::cout << std::endl;
-}
-
 bool equal(int a, int b) {
-  return (a<=b);
+  return (a==b);
 };
 
 int main() {
- 	ft::vector<int> vect;
+ 	ft::map<std::string, int> map;
 
-	vect.push_back(1);
-	vect.push_back(2);
-	vect.push_back(3);
+	map.insert(std::pair<std::string, int>("bbb", 19));
+	map.insert(std::pair<std::string, int>("aaa", 18));
+	map.insert(std::pair<std::string, int>("ccc", 20));
+	map.insert(std::pair<std::string, int>("ddd", 21));
+	std::cout << map.get_val() << std::endl;
+	std::cout << map._node->get_left() << std::endl;
+	std::cout << map._node->get_right() << std::endl;
+	std::cout << map._node->get_left()->get_left() << std::endl;
+	std::cout << map._node->get_right()->get_right() << std::endl;
 	
- 	ft::vector<int> vect2;
-
-	vect2.push_back(4);
-	vect2.push_back(5);
-	vect2.push_back(6);
-	
-
-	vect.insert(vect.begin(), vect2.begin(), vect2.end());
-
-	print_vect(vect);
-
-	vect.resize(size_t(10), 0);
-
-	print_vect(vect);
-
-	vect.erase(vect.begin(), vect.begin()+3);
-
-	print_vect(vect);
-
 }
---
+++ map.hpp
@@ -0,0 +1,155 @@
+#include "vector.hpp"
+
+namespace ft {
+
+	template <class Key, class T>
+	class	map_node
+	{
+	public:
+
+		typedef map_node<Key, T>		node;
+		typedef node *					node_ptr;
+		typedef const node *			const_node_ptr;
+		typedef std::pair<Key, T>		value_type;
+		typedef value_type				pair;
+
+		map_node() {
+			_top = NULL;
+			_left = NULL;
+			_right = NULL;
+		}
+
+		map_node(value_type const &pair) {
+			_keyval = pair;
+			_top = NULL;
+			_left = NULL;
+			_right = NULL;
+		}
+
+		map_node(value_type const &pair, node_ptr top) {
+			_keyval = pair;
+			_top = top;
+			_left = NULL;
+			_right = NULL;
+		}
+
+		map_node(map_node const &src) { *this = src; }
+
+		map_node &operator=(map_node const &src) {
+			_keyval = src._keyval;
+			_top = src._top;
+			_left = src._left;
+			_right = src._right;
+			return (*this);
+		}
+
+		virtual ~map_node() {}
+
+		void	set_top(const node_ptr node)	{ _top = node; };
+		void	set_left(const node_ptr node)	{ _left = node; };
+		void	set_right(const node_ptr node)	{ _right = node; };
+
+		const_node_ptr	get_curr(void) const  { return (this); };
+		node_ptr		get_top(void) const   { return (_top); };
+		node_ptr		get_left(void) const  { return (_left); };
+		node_ptr		get_right(void) const { return (_right); };
+
+		void	set_key(const Key &key)		{ _keyval.first = key; };
+		void	set_val(const T &val)		{ _keyval.second = val; };
+		void	set_pair(const value_type &val)	{ *_keyval = val; };
+
+		Key			&get_key(void) { return (_keyval.first); };
+		Key const	&get_key(void) const { return (_keyval.first); };
+		T			&get_val(void) { return (_keyval.second); };
+		pair		&get_pair(void) { return (_keyval); };
+		pair const	&get_pair(void) const { return (_keyval); };
+
+	private:
+
+		pair		_keyval;
+		node_ptr	_top;
+		node_ptr	_left;
+		node_ptr	_right;
+	};
+
+
+	template < class Key,                          //map::key_type
+	class T,                                       // map::mapped_type
+	class Compare = std::less<Key>,                     // map::key_compare
+	class Alloc = std::allocator<std::pair<const Key,T> >    // map::allocator_type
+	> class map {
+		
+		public :
+			
+			typedef Key														key_type;
+			typedef T														mapped_type;
+			typedef std::pair<const Key, T>									value_type;
+			typedef Compare													key_compare;
+			typedef Alloc													allocator_type;
+			typedef typename std::size_t 									size_type;
+			typedef typename std::ptrdiff_t 								difference_type;
+			typedef typename Alloc::const_reference							const_reference;
+			typedef typename Alloc::pointer									pointer;
+			typedef typename Alloc::const_pointer							const_pointer;
+			//typedef typename ft::map_iterator<T>							iterator;
+			//typedef typename ft::map_const_iterator<T>						const_iterator;
+			//typedef typename ft::map_reverse_iterator<iterator>				reverse_iterator;
+			//typedef typename ft::map_const_reverse_iterator<iterator>		const_reverse_iterator;
+
+			//typedef Compare												value_compare;
+
+			typedef map_node<Key, T>										node;
+			typedef node*													node_ptr;
+
+			explicit map (const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) {
+				_node = NULL;
+				_comp = comp;
+				_alloc = alloc;
+				_size = 0;
+			}
+
+			//std::pair<iterator,bool> insert (const value_type& val) {
+			std::pair<node_ptr,bool> insert (const value_type& val) {
+				if (!_node) {
+					_node = new node(val);
+					_size++;
+					//return std::pair<iterator,bool>(iterator(_node), true);
+					return std::pair<node_ptr,bool>(_node, true);
+				}
+				std::pair<bool, node_ptr> p = _search_node(_node, val.first);
+				bool already_exist = p.first;
+				node_ptr tmp = p.second;
+				if (already_exist)
+					return std::pair<node_ptr,bool>(tmp, false);
+				node_ptr new_node = new node(val, tmp);
+				if (_comp(val.first, tmp->get_key()))
+					tmp->set_left(new_node);
+				else
+					tmp->set_right(new_node);
+				_size++;
+				return std::pair<node_ptr,bool>(new_node, true);
+			}
+
+			T get_val() {return _node->get_val();}
+
+			node_ptr		_node;
+
+		private :
+
+			key_compare		_comp;
+			allocator_type	_alloc;
+			size_t			_size;
+
+			std::pair<bool, node_ptr> _search_node(node_ptr tmp, key_type key) {
+				if (tmp->get_key() == key)
+					return std::pair<bool, node_ptr>(true, tmp);
+				if ((!tmp->get_right() && _comp(tmp->get_key(), key)) || (!tmp->get_left() && _comp(key, tmp->get_key())))
+					return std::pair<bool, node_ptr>(false, tmp);
+				if (_comp(tmp->get_key(), key))
+					return _search_node(tmp->get_right(), key);
+				return _search_node(tmp->get_left(), key);
+			}
+
+	};
+
+}
